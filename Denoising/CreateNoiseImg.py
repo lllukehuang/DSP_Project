f31973206ccf96f268216e2ec2375d109ca82cf6
lllukehuang <LukeHuang@sjtu.edu.cn>
2020-12-11 00:58:44 +0800

    denoise test for different noises.

--- Denoising/CreateNoiseImg.py
+++ Denoising/CreateNoiseImg.py
@@ -3,7 +3,7 @@ import Adding_noise.noise as noise
 
 def main():
     imgpath = "Imgs/lena.jpg"
-    m = "random"
+    m = "pepper"
     cv2.imshow("Image", noise.noise(imgpath,"0"))
     cv2.imshow("Image_" + m, noise.noise(imgpath, m))
     cv2.imwrite("Imgs/Image_" + m + ".jpg", noise.noise(imgpath, m))
--- Denoising/Denoise.py
+++ Denoising/Denoise.py
@@ -4,43 +4,54 @@ import numpy as np
 import tools.tools as T
 
 origin_path = "Imgs/lena.jpg"
-img_path = "Imgs/Image_random.jpg"
+img_path = "Imgs/Image_pepper.jpg"
 img_save_path = "Imgs/"
-
+logfile_path = "Imgs/PSNR.txt"
+f = open(logfile_path,"w",encoding='utf=8')
 # 读取灰度图片
 origin_img = cv2.imread(origin_path,0)
 img = cv2.imread(img_path,0)
 print("原始的峰值信噪比：",T.psnr(origin_img,img))
+f.write("原始的峰值信噪比："+str(T.psnr(origin_img,img)))
+f.write("\n")
 # Gaussian Filter 高斯滤波
 out = sp.gauss.gaussian_filter(img, kernel_size=3, sigmax=1.3)
 # Save result
 cv2.imwrite(img_save_path + "gauss_out.jpg", out)
 # cv2.imshow("gauss_result", out)
 print("高斯滤波后的峰值信噪比：",T.psnr(origin_img,out))
-
-
+f.write("高斯滤波后的峰值信噪比："+str(T.psnr(origin_img,out)))
+f.write("\n")
 # median filter 中值滤波
 out = sp.median.MedianFilter(img_path,img_save_path + "Median_out.jpg",k=3,padding = None)
 print("中值滤波后的峰值信噪比：",T.psnr(origin_img,out))
+f.write("中值滤波后的峰值信噪比："+str(T.psnr(origin_img,out)))
+f.write("\n")
 # better median filter
 out = sp.betterMedian.BetterMedianFilter(img_path,img_save_path + "BetterMedian_out.jpg",k=3,padding = None)
 print("优化后中值滤波后的峰值信噪比：",T.psnr(origin_img,out))
+f.write("优化后中值滤波后的峰值信噪比："+str(T.psnr(origin_img,out)))
+f.write("\n")
 # bilateral filter 双边滤波
 out = cv2.bilateralFilter(src=img, d=0, sigmaColor=100, sigmaSpace=15)
 cv2.imwrite(img_save_path + "bilateral_out.jpg", out)
 print("双边滤波后的峰值信噪比：",T.psnr(origin_img,out))
-
+f.write("双边滤波后的峰值信噪比："+str(T.psnr(origin_img,out)))
+f.write("\n")
 # NLmean 非局部均值滤波
 sigma = 20
 out = cv2.fastNlMeansDenoising(img, None, sigma, 5, 11)
 cv2.imwrite(img_save_path + "opencvNLmean_out.jpg", out)
 print("opencv非局部均值滤波后的峰值信噪比：",T.psnr(origin_img,out))
+f.write("opencv非局部均值滤波后的峰值信噪比："+str(T.psnr(origin_img,out)))
+f.write("\n")
 out = sp.NLmean.NLmeansfilter(img.astype(np.float), sigma, 5, 11)
 out = T.double2uint8(out)
 cv2.imwrite(img_save_path + "NLmean_out.jpg", out)
 print("优化后非局部均值滤波后的峰值信噪比：",T.psnr(origin_img,out))
-
-
+f.write("优化后非局部均值滤波后的峰值信噪比："+str(T.psnr(origin_img,out)))
+f.write("\n")
+f.close()
 print("图片已保存至"+img_save_path+"文件夹下")
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()

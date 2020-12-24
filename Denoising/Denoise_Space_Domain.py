9fe26b14ef2fafc2124134544459cc6ece7ed70a
lllukehuang <LukeHuang@sjtu.edu.cn>
2020-12-24 20:04:54 +0800

    change the architecture of whole codes

--- Denoising/Denoise_Space_Domain.py
+++ Denoising/Denoise_Space_Domain.py
@@ -4,7 +4,7 @@ import numpy as np
 import tools.tools as T
 
 origin_path = "Imgs/lena.jpg"
-img_path = "Imgs/Image_salt.jpg"
+img_path = "Imgs/Image_s&p.jpg"
 img_save_path = "Imgs/"
 logfile_path = "Imgs/PSNR.txt"
 f = open(logfile_path,"w",encoding='utf=8')

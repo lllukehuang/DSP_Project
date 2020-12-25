8b697507b37d8d1562c55dbc4a7e81ab84b528f8
lllukehuang <LukeHuang@sjtu.edu.cn>
2020-12-25 15:21:32 +0800

    change parameters and add report images generating codes

--- Denoising/frequency_denoise_report_imgs.py
+++ Denoising/frequency_denoise_report_imgs.py
@@ -5,7 +5,8 @@ import tools.tools as T
 import matplotlib.pyplot as plt
 
 origin_path = "Imgs/lena.jpg"
-img_path = "Imgs/Image_s&p.jpg"
+noise = "gaussian"
+img_path = "Imgs/Image_" + noise + ".jpg"
 img_save_path = "Imgs/"
 logfile_path = "Imgs/PSNR.txt"
 
@@ -19,45 +20,56 @@ print("原始的峰值信噪比：",T.psnr(origin_img,img))
 out = sp.gauss.gaussian_filter(img, kernel_size=3, sigmax=0)
 print("高斯滤波后的峰值信噪比：",T.psnr(origin_img,out))
 
-plt.imshow(out)
-plt.subplot(1,2,1)
+ax1 = plt.subplot(1,2,1)
+ax1.set_title("Gauss Filtering Result")
+plt.imshow(out,cmap="gray")
 
 # median filter 中值滤波
 out = sp.median.MedianFilter(img_path,img_save_path + "Median_out.jpg",k=3,padding = None)
 print("中值滤波后的峰值信噪比：",T.psnr(origin_img,out))
 
-plt.imshow(out)
-plt.subplot(1,2,2)
-
+ax2 = plt.subplot(1,2,2)
+ax2.set_title("Median Filtering Result")
+plt.imshow(out,cmap="gray")
+# plt.title(noise+" denoising results")
+plt.show()
+plt.figure()
 # better median filter
 out = sp.betterMedian.BetterMedianFilter(img_path,img_save_path + "BetterMedian_out.jpg",k=3,padding = None)
 print("优化后中值滤波后的峰值信噪比：",T.psnr(origin_img,out))
 
-plt.imshow(out)
-plt.subplot(1,2,1)
+ax1 = plt.subplot(1,2,1)
+ax1.set_title("Optimized Median Filtering Result")
+plt.imshow(out,cmap="gray")
 
 # bilateral filter 双边滤波
-out = cv2.bilateralFilter(src=img, d=0, sigmaColor=100, sigmaSpace=1)
+out = cv2.bilateralFilter(src=img, d=0, sigmaColor=200, sigmaSpace=1)
 # cv2.imwrite(img_save_path + "bilateral_out.jpg", out)
 print("双边滤波后的峰值信噪比：",T.psnr(origin_img,out))
 
-plt.imshow(out)
-plt.subplot(1,2,2)
-
+ax2 = plt.subplot(1,2,2)
+ax2.set_title("Bilateral Filtering Result")
+plt.imshow(out,cmap="gray")
+# plt.title(noise+" denoising results")
+plt.show()
+plt.figure()
 # NLmean 非局部均值滤波
 sigma = 20
 out = cv2.fastNlMeansDenoising(img, None, sigma, 5, 11)
 # cv2.imwrite(img_save_path + "opencvNLmean_out.jpg", out)
 print("opencv非局部均值滤波后的峰值信噪比：",T.psnr(origin_img,out))
 
-plt.imshow(out)
-plt.subplot(1,2,1)
+ax1 = plt.subplot(1,2,1)
+ax1.set_title("Opencv NLmean Filtering Result")
+plt.imshow(out,cmap="gray")
 
 out = sp.NLmean.NLmeansfilter(img.astype(np.float), sigma, 5, 11)
 out = T.double2uint8(out)
 # cv2.imwrite(img_save_path + "NLmean_out.jpg", out)
 print("优化后非局部均值滤波后的峰值信噪比：",T.psnr(origin_img,out))
 
-plt.imshow(out)
-plt.subplot(1,2,2)
+ax2 = plt.subplot(1,2,2)
+ax2.set_title("NLmean Filtering Result")
+plt.imshow(out,cmap="gray")
+# plt.title(noise+" denoising results")
 plt.show()
